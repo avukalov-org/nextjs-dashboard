feb33150e224d32bce31c911a44f6187830fd483
Antonio Vukalovic <a.vukalovic991@gmail.com>
2024-02-25 17:14:17 +0100

    fix overflow hidden

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className="overflow-hidden">
+    <html lang="en">
       <body className={`${inter.className} antialiased`}>
         <UserProvider>
           <ApolloWrapper>
